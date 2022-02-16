04ab3e962b4c6a6fa3e3711408077ce6fc658846
rimutaka <max@onebro.me>
2022-02-17 12:31:56 +1300

    Delete redundant files from public dir

--- deploy.sh
+++ deploy.sh
@@ -1,1 +1,8 @@
+# remove files that do not need to be deployed
+# I do not know how to prevent them from being generated
+rm -rf ./public/content
+rm ./public/404.html
+rm ./public/robots.txt
+rm ./public/sitemap.xml
+# push to S3
 aws s3 rm s3://cv.rimutaka.me/ --recursive  --profile onebro && aws s3 cp ./public/  s3://cv.rimutaka.me/ --recursive  --profile onebro
